dfc423dee5caa660d481da4f57867b876867efa6
unknown <MARCOS_24046@lab.etec187>
2026-08-25 09:35:11 -0300

    Criação da tela header

---
+++ app/Views/header/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/TCC_reposit/CSS/header.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header"> 
+        
+        <div class="logo_area">
+        <a href="/TCC_reposit/">
+            <img src="/TCC_reposit/img/logo.png" alt="Logo do Projeto" class="logo">
+        </a>
+        <h2 class="nome_empresa">FastService</h2>
+    </div>
+        <nav class="cabecalho">
+            <a href="" cla>Funcionamento</a>
+            <a href="">Serviços</a>
+            <a href="">Seja um Profissional</a>
+            <a href="">
+            <img src="/TCC_reposit/img/.png" alt="" class="">
+            </a>
+            <a href="">Login</a>
+        </nav>
+    </header>
+</body>
+</html>
--- app/Views/home.php
+++ app/Views/home.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/TCC_reposit/css/home.css">
     <title>home</title>
 </head>
 <body>
-    <h1>oiiiiiiiiiii</h1>
+    <?php require_once __DIR__ . '/header/header.php'; ?>
+    <h1 class="a">oiiiiiiiiiii</h1>
     <a href="?route=cadastro-form">cadastro</a>
 </body>
 </html>
