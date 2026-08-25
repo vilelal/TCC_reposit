f0202fd3cffcdaac5224cc89fa197ebbc7531e3b
unknown <REBECCA_24048@lab.etec187>
2026-08-25 09:28:47 -0300

    Css do home

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,10 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/TCC_reposit/css/style.css">
     <title>home</title>
 </head>
 <body>
-    <h1>oiiiiiiiiiii</h1>
+    <section class="hero-section">
+    
+    <div class="conteiner">
+        <br><br><br><br><br><br>
+            <h1 class="h1">O profissional certo em poucos cliques. <a href="" class="a">Contrate já</a></h1>
+    <p>Segurança e rapidez em encontrar o profissional ideal para suas necessidades.</p>
+    <br>
+    <br>
     <a href="?route=cadastro-form">cadastro</a>
+    <form action="" method="POST">
+        <input type="text" name="nome" placeholder="Digite o nome do serviço">
+        <br>
+        <button type="submit">Pesquisar</button>
+    </form>
+    
+    </div>
+        <section class="categories-section">
+        <h3>Profissionais Por Categoria</h3>
+        <div class="categories-grid">
+            <div class="category-card">
+                <img src="" alt="Assistência técnica">
+                <span><a href="">Assistência técnica geral</a></span>
+            </div>
+            <div class="category-card">
+                <img src="" alt="Pequenos concertos">
+                <span><a href="">Pequenos Concertos</a></span>
+            </div>
+            <div class="category-card">
+                <img src="" alt="Reformas">
+                <span><a href="">Reformas e Reparos</a></span>
+            </div>
+            <div class="category-card">
+                <img src="" alt="Serviços gerais">
+                <span><a href="">Serviços Em Geral</a></span>
+            </div>
+            <div class="category-card">
+                <img src="" alt="Serviços domésticos">
+                <span><a href="">Serviços Domésticos</a></span>
+            </div>
+            <div class="category-card">
+                <img src="" alt="Meio automotivo">
+                <span><a href="">Meio Automotivo</a></span>
+            </div>
+        </div>
+    </section>
+
+    </section>
+
+
 </body>
 </html>
