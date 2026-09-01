0d20b8d857d3b0d23e57bd340447dbff611ba826
unknown <MARCOS_24046@lab.etec187>
2026-09-01 09:39:10 -0300

    Feito header, resolvendo problema com páginas require

--- app/Views/header/header.php
+++ app/Views/header/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/TCC_reposit/CSS/header.css">
+     <link rel="stylesheet" href="../../css/header.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;600;700;800&display=swap" rel="stylesheet">
@@ -14,18 +14,21 @@
         
         <div class="logo_area">
         <a href="/TCC_reposit/">
-            <img src="/TCC_reposit/img/logo.png" alt="Logo do Projeto" class="logo">
+            <img src="../../css/img/logo.png" alt="" class="logo">
         </a>
         <h2 class="nome_empresa">FastService</h2>
     </div>
         <nav class="cabecalho">
-            <a href="" cla>Funcionamento</a>
-            <a href="">Serviços</a>
-            <a href="">Seja um Profissional</a>
+            <div class="separar">
+            <a href="" style="font-size: clamp(0.8rem, 1.07vw, 3.5rem); margin-left:" class="funciona,ento">Funcionamento</a>
+            <a href="" style="font-size: clamp(0.8rem, 1.07vw, 3.5rem);" class="servico">Serviços</a>
+                
+            </div>
+            <a href="" class="btn-profissional">Seja um Profissional</a>
             <a href="">
-            <img src="/TCC_reposit/img/.png" alt="" class="">
+            <img src="../../css/img/chat.png" alt="" class="chat">
             </a>
-            <a href="">Login</a>
+            <a href="" class="btn-login">Login</a>
         </nav>
     </header>
 </body>
