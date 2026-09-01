6f76dbabe177d481f1fae70bda252b6d8f27fbdf
unknown <VITOR_24054@lab.etec187>
2026-09-01 09:38:06 -0300

    cadastro de prestador

--- app/Views/home.php
+++ app/Views/home.php
@@ -16,6 +16,7 @@
     <br>
     <br>
     <a href="?route=cadastro-form">cadastro</a>
+    <a href="?route=prestador-form">cadastro prestador</a>
     <?php 
     if (isset($_SESSION["id"])) {
         echo "<a href='?route=logout'>logout</a>";
