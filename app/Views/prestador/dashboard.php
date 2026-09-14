d4197ea64e6b3d3b38885fbf2fa022040b2996ba
staybecca <rebecca.s.santana2017@gmail.com>
2026-09-14 18:34:43 -0300

    teste

--- app/Views/prestador/dashboard.php
+++ app/Views/prestador/dashboard.php
@@ -3,10 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app/css/dashboard.css">
     <title>Dashboard</title>
 </head>
-<body>
-    <a href="?route=lista-servicos">serviços</a>
-    <a href="?route=perfil">perfil</a>
+<body>  
+        <div class="menu">
+            <div class="conteudo">
+                <div class="titulo">
+              <a href="?route=">
+                <img src="app/css/img/logo.png" alt="" class="logo">
+                </a>
+        <h1 class="nome_empresa">FastService</h1> </div>
+       
+                 <a class="menu-a" href="?route=dashboard"><img class="casa" src="app/css/img_dashboard/icone-casa.png" alt=""><h2>Inicio</h2></a>
+                  <a class="menu-a" href="?route=dashboard"><img src="" alt=""><h2>Relatório</h2></a>
+                <a class="menu-a" href="?route=lista-servicos"><img src="" alt=""><h2>Serviços</h2></a>
+                <a class="menu-a" href="?route=perfil"><img src="" alt=""><h2>Perfil</h2></a>
+                </div>
+        </div>
+       <header class="header">
+            <div class="header-right">
+                <a href="">
+                    <img src="app/css/img/chat.png" alt="" class="chat">
+                </a>
+                <a href="">
+                    <img src="app/css/img/icone-sino.png" alt="" class="chat">
+                </a>
+                <img src="" alt="">
+                <?php
+                    if (isset($_SESSION["id"])) {
+                        echo "<h3 class='user-name'>{$_SESSION['nome']}</h3>";
+                    }
+                ?>
+            </div>
+        </header>
+        <div class="conteiner">
+            
+        </div>
+
 </body>
 </html>
